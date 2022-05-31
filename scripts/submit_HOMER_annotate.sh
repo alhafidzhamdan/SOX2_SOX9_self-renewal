ae96e22c18e4ce714ca0fc26c3ae02fd9405c9d5
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2022-05-31 09:21:00 +0100

    Update submit_HOMER_annotate.sh

--- scripts/submit_HOMER_annotate.sh
+++ scripts/submit_HOMER_annotate.sh
@@ -25,19 +25,10 @@ BED_FILE=`head -n $SGE_TASK_ID $BED_LIST | cut -f 2 | tail -n 1`
 
 #### Annotate (need to use unzipped fasta file for the reference, and unzipped gencode gtf file)
 #### Based on http://homer.ucsd.edu/homer/ngs/annotation.html
-annotatePeaks.pl $BED_FILE mm10 -CpG -annStats $HOMER_DIR/${SAMPLE_ID}.annotated.stats.txt  > $HOMER_DIR/${SAMPLE_ID}.annotated.txt
-
-#### Subset
-##cat $HOMER_DIR/${SAMPLE_ID}.annotated.txt | grep -si promoter | cut -f 2-4 | grep -vsi start > $HOMER_DIR/${SAMPLE_ID}.annotated.promoter.bed
-##cat $HOMER_DIR/${SAMPLE_ID}.annotated.txt | grep -si intergenic | cut -f 2-4 | grep -vsi start > $HOMER_DIR/${SAMPLE_ID}.annotated.intergenic.bed
-##cat $HOMER_DIR/${SAMPLE_ID}.annotated.txt | grep -vsi intergenic | grep -vsi promoter | cut -f 2-4 | grep -vsi start > $HOMER_DIR/${SAMPLE_ID}.annotated.intragenic.bed
-    
+annotatePeaks.pl $BED_FILE hg38 -CpG -annStats $HOMER_DIR/${SAMPLE_ID}.annotated.stats.txt  > $HOMER_DIR/${SAMPLE_ID}.annotated.txt
+ 
 #### Convert to fasta
-bedtools getfasta -fi $MM10_REF_FASTA -bed $BED_FILE > $FASTA_DIR/${SAMPLE_ID}.fasta
-##bedtools getfasta -fi $MM10_REF_FASTA -bed $HOMER_DIR/${SAMPLE_ID}.annotated.promoter.bed > $FASTA_DIR/${SAMPLE_ID}.annotated.promoter.fasta
-##bedtools getfasta -fi $MM10_REF_FASTA -bed $HOMER_DIR/${SAMPLE_ID}.annotated.intergenic.bed > $FASTA_DIR/${SAMPLE_ID}.annotated.intergenic.fasta
-##bedtools getfasta -fi $MM10_REF_FASTA -bed $HOMER_DIR/${SAMPLE_ID}.annotated.intragenic.bed > $FASTA_DIR/${SAMPLE_ID}.annotated.intragenic.fasta
-
+bedtools getfasta -fi $HG38_REFERENCE -bed $BED_FILE > $FASTA_DIR/${SAMPLE_ID}.fasta
 
 
 
