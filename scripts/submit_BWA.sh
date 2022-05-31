e21203e4904cfd0b80244289c6cc0e10fc07ab0c
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2022-05-31 09:21:33 +0100

    Update and rename submit_bwa.sh to submit_BWA.sh

--- scripts/submit_BWA.sh
+++ scripts/submit_BWA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # To run this script, do 
-# qsub -t 1-n submit_bwa.sh CONFIG IDS
+# qsub -t 1-n submit_BWA.sh CONFIG IDS
 #
 # CONFIG is the path to the file scripts/config.sh which contains environment variables set to
 # commonly used paths and files in the script
@@ -26,13 +26,13 @@ source $CONFIG
 
 SAMPLE_ID=`head -n $SGE_TASK_ID $IDS | tail -n 1 | cut -f 8`
 
-# First run `bwa index $BWA_REF`
+# First run `bwa index $HG38_REFERENCE`
 
 # Align fastq reads 
 
 cd $READ_DIR
 echo "Aligning fastq file for ${SAMPLE_ID} ... "
-bwa mem -M -t 20 $BWA_REF ${SAMPLE_ID}_R1.fastq.gz ${SAMPLE_ID}_R2.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
+bwa mem -M -t 20 $HG38_REFERENCE ${SAMPLE_ID}_R1.fastq.gz ${SAMPLE_ID}_R2.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
 samtools index $BAM_DIR/${SAMPLE_ID}.sorted.bam
 samtools flagstat $BAM_DIR/${SAMPLE_ID}.sorted.bam > $BAM_DIR/${SAMPLE_ID}.stats.out
 
