0a66da2e75b83455a630a08d1f2d1a4f387cd252
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2022-05-31 09:17:08 +0100

    Update submit_homer_motif.sh

--- scripts/submit_homer_motif.sh
+++ scripts/submit_homer_motif.sh
@@ -21,17 +21,13 @@ source $CONFIG
 
 SAMPLE_ID=`head -n $SGE_TASK_ID $BED_LIST | cut -f 1 | tail -n 1`
 BED_FILE=`head -n $SGE_TASK_ID $BED_LIST | cut -f 2 | tail -n 1`
-##BED_FILE=/exports/igmm/eddie/Glioblastoma-WGS/ChIP-seq/SOX_TF/downstream/beds/$BED
 RESULT_DIR=${OUTDIR}/${SAMPLE_ID}/
 
 ## Get enriched motifs
 findMotifsGenome.pl \
      $BED_FILE \
-     mm10 \
+     hg38 \
      $RESULT_DIR \
      -size given
      
-###scanMotifGenomeWide.pl $MOTIF $INPUT_DIR -keepAll -homer2 -bed > ${SAMPLE_ID}_homer_called_motif.bed
-### Use scrambleFasta.pl to generate background sequence
 
-     
